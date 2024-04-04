9f8a7ce6ab9489ee0d7e6725959b93950d7be327
unknown <146474490+AfroOrange@users.noreply.github.com>
2024-04-04 10:08:54 +0100

    Actualiazdo con comentarios

--- src/user.java
+++ src/user.java
@@ -1,11 +1,13 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/* Clase usuario sobre la cual haremos las condiciones de los errores */
 public class user {
     private String nombre;
     private int edad;
     private double altura;
 
+    /* En el constructor definiremos que tanto la altura como la edad no pueda ser negativa */
     public user(String nombre, int edad, double altura) {
         Scanner scanner = new Scanner(System.in);
 
@@ -27,7 +29,7 @@ public class user {
                 try {
                     System.out.println("No se puede introducir una altura igual o menor que cero");
                     altura = scanner.nextInt();
-                } catch (InputMismatchException e) {
+                } catch (InputMismatchException e) { // esta línea no funciona correctamente
                     scanner.nextLine();
                     System.out.println("Error: Ingrese un número válido para la edad:");
                 } catch (Exception e) {
@@ -37,6 +39,8 @@ public class user {
             this.altura = altura;
         }
     }
+
+    // Método para mostrar los datos del usuario registrado
     public void mostrar_datos() {
         System.out.println("Nombre: " + this.nombre);
         System.out.println("Edad:   " + this.edad);
